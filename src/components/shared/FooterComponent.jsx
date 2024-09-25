07a864ea8c7365363fdaa0ca092a7d70a2ed4cd6
RohitSharmagit11 <rohit.sharma458@gmail.com>
2024-09-25 18:07:17 +0530

    added rightside bar content with open close functionality

--- src/components/shared/FooterComponent.jsx
+++ src/components/shared/FooterComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FooterComponent = () => {
   return (
-    <div className='echt-footer' >
+    <div className='echt-footer px-3' >
       Footer
     </div>
   )
--- src/components/shared/SidebarComponent.jsx
+++ src/components/shared/SidebarComponent.jsx
@@ -1,69 +1,21 @@
 import React from 'react'
 import logo from "../../assetsechttech/logo/logo.png"
-import { useState } from 'react'
-import { Link } from 'react-router-dom'
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined'
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
+import { AiOutlineDashboard } from "react-icons/ai";
+import SidebarcontentComponent from './SidebarcontentComponent'
 
  
 const SidebarComponent = () => {
   return (
-    <div className='left-sidebar-inner p-4'>
-       <div className="sidebar-top-logo-con w-full rounded-lg logo-custom-border p-2">
+    <div className='left-sidebar-inner'>
+       <div className="sidebar-top-logo-con w-full rounded-lg logo-custom-border p-4 mt-8">
          <img src={logo} alt="" />
        </div>
-       <div className="below-content mt-8">
-         <h3 className='text-white mb-10 dashboard-text' ><DashboardOutlinedIcon className='color-white mr-3' fontSize="small" />Dashboard</h3>
-         {/* <div className="listing">
-          <ul>
-            <li className='list'><Link to="" className='text-base'>Attendance<ArrowForwardIosOutlinedIcon className='arrow-icons' /></Link>
-            <ul>
-              <li className='list'><Link to="">demo1</Link></li>
-              <li className='list'><Link to="">demo2</Link></li>
-              <li className='list'><Link to="">demo3</Link></li>
-              <li className='list'><Link to="">demo4</Link></li>
-            </ul>
-            </li>
-            <li className='list'><Link to="">Payroll<ArrowForwardIosOutlinedIcon className='arrow-icons' /></Link>
-            <ul>
-            <li className='list'><Link to="">demo1</Link></li>
-              <li className='list'><Link to="">demo2</Link></li>
-              <li className='list'><Link to="">demo3</Link></li>
-              <li className='list'><Link to="">demo4</Link></li>
-            </ul>
-            </li>
-          </ul>
-         </div> */}
-        <Accordion className='ul-list'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1-content"
-          id="panel1-header"
-        >
-          Attendance
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
-      <Accordion className='ul-list'>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2-content"
-          id="panel2-header"
-        >
-          Payroll
-        </AccordionSummary>
-        <AccordionDetails>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </AccordionDetails>
-      </Accordion>
+       <div className="below-content mt-8 p-4 ">
+        <div className="dash-icon flex ml-6">
+          <AiOutlineDashboard  className='color-white mr-3 text-xl'/>
+          <h1 className='text-white mb-3 dashboard-text' >Dashboard</h1>
+         </div>
+         <SidebarcontentComponent />
       </div>
     </div>
   )
