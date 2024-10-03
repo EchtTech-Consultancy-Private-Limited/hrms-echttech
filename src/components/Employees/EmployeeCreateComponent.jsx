196757019a1351f555ce75cdb8e1493fb0dee591
brijesh-echttech <brijesh.sharma@echttech.com>
2024-10-03 18:35:15 +0530

    bgu fix

---
+++ src/components/Employees/EmployeeCreateComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmployeeCreateComponent = () => {
+  return (
+    <div>
+        EmployeeCreateComponent
+    </div>
+  )
+}
+
+export default EmployeeCreateComponent
---
+++ src/pages/EmployeesPages/EmployeeCreatePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import EmployeeCreateComponent from '../../components/Employees/EmployeeCreateComponent'
+
+const EmployeeCreatePage = () => {
+  return (
+    <div>
+        <EmployeeCreateComponent />
+    </div>
+  )
+}
+
+export default EmployeeCreatePage
