7bc3720a7571a76824bbe86c50a347d4fed7aaa9
Prachi Verma <prachi.verma@echttech.com>
2024-10-17 17:49:28 +0530

    fixed new componentimg upload.jsx

--- src/pages/SettingPages/SystemLogoPage.jsx
+++ src/pages/SettingPages/SystemLogoPage.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import SystemLogoComponent from '../../components/Settings/SystemLogoComponent'
+import React from "react";
+import SystemLogoComponent from "../../components/Settings/SystemLogoComponent";
 
 const SystemLogoPage = () => {
   return (
-    <div>
-      <SystemLogoComponent />
+    <div className="content-mid">
+      <div id="dashboard" className="px-5 py-12 right-const-structure">
+        <SystemLogoComponent />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SystemLogoPage
+export default SystemLogoPage;
